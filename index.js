391d6b965bfaf2555ae63660a9fa35b4ba45f621
Wahab-Malik <malikwahab34@gmail.com>
2023-07-02 18:34:51 +0500

    Added Update operation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./routes/routes");
 const connectToDatabase = require("./database");
 const Product = require("./models/productModels");
-
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use("/api", router);
 app.get("/", (req, res) => {
   res.send("Helloworld");
 });
-
+//CURD CREATE
 app.post("/product", async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -21,11 +21,30 @@ app.post("/product", async (req, res) => {
   }
 });
 
+//CURD READ
 app.get("/products", async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
   } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//CURD UPDATE
+app.put("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error.message);
     res.status(500).json({ message: error.message });
   }
 });
