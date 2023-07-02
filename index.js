d46dfd2d094ff7730a3312a73a82a7a7aec8266b
Wahab-Malik <malikwahab34@gmail.com>
2023-07-02 16:10:51 +0500

    Added Create operation

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = require("./routes/routes");
 const connectToDatabase = require("./database");
+const Product = require("./models/productModels");
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 app.use("/api", router);
 app.get("/", (req, res) => {
   res.send("Helloworld");
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 connectToDatabase()
