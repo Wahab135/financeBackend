51619b111bbda40f8648f3821d4fd23d7f1256db
Wahab-Malik <malikwahab34@gmail.com>
2023-07-02 15:19:33 +0500

    Added Database connection

--- database.js
+++ database.js
@@ -1,1 +1,15 @@
 //connectionString = mongodb+srv://wahab:<password>@nodeapitesting.8brpasx.mongodb.net/?retryWrites=true&w=majority
+const mongoose = require("mongoose");
+const connectToDatabase = async () => {
+  mongoose
+    .connect(
+      "mongodb+srv://wahab:03315046101@nodeapitesting.8brpasx.mongodb.net/Node-API?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log("connected to mongodb");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+module.exports = connectToDatabase;
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = require("./routes/routes");
+const connectToDatabase = require("./database");
+
 const app = express();
 const port = 3000;
 app.use("/api", router);
-
 app.get("/", (req, res) => {
   res.send("Helloworld");
 });
 
-app.listen(port, () => {
-  console.log(`listening on port : ${port}`);
-});
+connectToDatabase()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening on port : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
